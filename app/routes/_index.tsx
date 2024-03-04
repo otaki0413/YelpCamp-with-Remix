d322580e9a0a121f65dcf3c5a73a30b108ca4591
otaki0413 <takida0413@gmail.com>
2024-03-05 00:08:16 +0900

    💄 #29 不要なプロパティ削除

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,17 +11,16 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="relative px-6 pt-14 lg:px-8">
+    <div className="px-6 pt-16">
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
-        <div className="text-center">
+        <div className="text-center sm:mb-8">
           <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
             YelpHotSpring
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             日本各地の温泉を紹介、ユーザー同士でレビューできるアプリです
           </p>
-          <div className="mt-8 flex items-center justify-center gap-x-6">
+          <div className="mt-8">
             <Button asChild variant="outline">
               <Link to="/hotsprings/">温泉リストへ</Link>
             </Button>
