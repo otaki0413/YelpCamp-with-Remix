4fac7651b6e438494f0c1dd30965946b68f45ead
otaki0413 <takida0413@gmail.com>
2024-03-09 15:39:47 +0900

    🎨 #47 レビューフォーム送信後にフォーム内容をリセットできるようにする

---
+++ app/components/Rating/RatingForm.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+type Props = {
+  rating: number;
+  setRating: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const RatingForm: React.FC<Props> = ({ rating, setRating }) => {
+  const [hoveredRating, setHoveredRating] = useState(0);
+
+  return (
+    <div className="flex items-center">
+      {[1, 2, 3, 4, 5].map((value) => (
+        <div
+          key={value}
+          onClick={() => setRating(value)}
+          onMouseEnter={() => setHoveredRating(value)}
+          onMouseLeave={() => setHoveredRating(0)}
+          className="relative cursor-pointer"
+        >
+          <input
+            type="radio"
+            name="rating"
+            value={value}
+            checked={value === rating}
+            onChange={() => setRating(value)}
+            className="hidden"
+          />
+          <span
+            className={`text-3xl ${
+              value <= (hoveredRating || rating)
+                ? "text-yellow-500"
+                : "text-gray-500"
+            }`}
+          >
+            &#9733;
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
